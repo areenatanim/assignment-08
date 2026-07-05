d97892b323c616a71f27ea3027c9cde1ab6c0c01
areenatanim <areenatanim@gmail.com>
2026-07-05 20:29:47 +0600

    speed added

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,8 +1,9 @@
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
 const HomePage = () => {
   return (
     <div>
       <h2>this is home page</h2>
+      <SpeedInsights />
     </div>
   );
 };
